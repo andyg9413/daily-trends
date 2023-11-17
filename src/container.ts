93498c2263f88c0ebe5311697f3bebff935a5f8b
andyglez96 <andy.gonzalez@opstalent.com>
2023-11-18 00:30:46 +0100

    feat: adding cron to schedule the scrapping at 6:00 every day

--- src/container.ts
+++ src/container.ts
@@ -3,6 +3,7 @@ import { FeedService } from './services/feed.service';
 import { FeedRepository } from './repositories/feed.repository';
 import { FeedEntity } from './entities/feed.entity';
 import { FeedController } from './controllers/feed.controller';
+import {ScrapCron} from "./scrapping/scrap.cron";
 
 const container = createContainer();
 
@@ -11,6 +12,7 @@ container.register({
   feedRepository: asClass(FeedRepository).singleton(),
   feedEntity: asClass(FeedEntity),
   feedController: asClass(FeedController).singleton(),
+  scrapCron: asClass(ScrapCron).singleton(),
 });
 
 export { container };
