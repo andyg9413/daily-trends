372b7bf81bd9a12245a13c4c195527ea35131e31
andyglez96 <andy.gonzalez@opstalent.com>
2023-11-18 22:44:52 +0100

    refactor: passing lint

--- src/test/scrapper.service.spec.ts
+++ src/test/scrapper.service.spec.ts
@@ -1,62 +1,83 @@
-import {ElpaisService} from "../scrapping/elpais.service";
+import { ElpaisService } from '../scrapping/elpais.service';
 import { IArticle } from '../scrapping/article.interface';
-import { IFeed } from '../entities/feed.interface';
-import { mapArticleToFeed } from '../scrapping/scrapper.transform';
 import { ScrapperService } from '../scrapping/scrapper.service';
-import {ElmundoService} from "../scrapping/elmundo.service";
-import {FeedRepository} from "../repositories/feed.repository";
-import {FeedEntity} from "../entities/feed.entity";
-import {FeedService} from "../services/feed.service";
+import { ElmundoService } from '../scrapping/elmundo.service';
+import { FeedRepository } from '../repositories/feed.repository';
+import { FeedEntity } from '../entities/feed.entity';
+import { FeedService } from '../services/feed.service';
 
 jest.mock('../scrapping/news-site.service', () => {
-    const originalModule = jest.requireActual('../scrapping/news-site.service');
-    const mockReadFeeds = jest.fn();
+  const originalModule = jest.requireActual('../scrapping/news-site.service');
+  const mockReadFeeds = jest.fn();
 
-    return {
-        ...originalModule,
-        NewsSite: jest.fn(() => ({
-            readFeeds: mockReadFeeds,
-        })),
-    };
+  return {
+    ...originalModule,
+    NewsSite: jest.fn(() => ({
+      readFeeds: mockReadFeeds,
+    })),
+  };
 });
 
 const mockFeedRepository = new FeedRepository({
-    feedEntity: new FeedEntity(),
+  feedEntity: new FeedEntity(),
 }) as jest.Mocked<FeedRepository>;
 
 const mockFeedService = new FeedService({
-    feedRepository: mockFeedRepository,
+  feedRepository: mockFeedRepository,
 }) as jest.Mocked<FeedService>;
 
-const mockElpaisService = new ElpaisService({feedService: mockFeedService}) as jest.Mocked<ElpaisService>;
-const mockElmundoService = new ElmundoService({feedService: mockFeedService}) as jest.Mocked<ElmundoService>;
+const mockElpaisService = new ElpaisService({
+  feedService: mockFeedService,
+}) as jest.Mocked<ElpaisService>;
+const mockElmundoService = new ElmundoService({
+  feedService: mockFeedService,
+}) as jest.Mocked<ElmundoService>;
 
 const scrapperService = new ScrapperService({
-    elpaisService: mockElpaisService,
-    elmundoService: mockElmundoService,
+  elpaisService: mockElpaisService,
+  elmundoService: mockElmundoService,
 }) as jest.Mocked<ScrapperService>;
 
 describe('ScrapperService', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    it('should get and save all feeds without limit', async () => {
-        const mockElpaisArticles: IArticle[] = [
-            { title: 'Elpais Title 1', body: 'Elpais Body 1', link: 'https://elpais.com/1.png', image: 'https://image.elpais.com/1.png' },
-            { title: 'Elpais Title 2', body: 'Elpais Body 2', link: 'https://elpais.com/2.png', image: 'https://image.elpais.com/1.png' },
-        ];
-        const mockElmundoArticles: IArticle[] = [
-            { title: 'Elmundo Title 1', body: 'Elmundo Body 1', link: 'https://elmundo.es/1.png', image: 'https://image.elmundo.es/1.png' },
-            { title: 'Elmundo Title 2', body: 'Elmundo Body 2', link: 'https://elmundo.es/2.png', image: 'https://image.elmundo.es/2.png' },
-        ];
+  it('should get and save all feeds without limit', async () => {
+    const mockElpaisArticles: IArticle[] = [
+      {
+        title: 'Elpais Title 1',
+        body: 'Elpais Body 1',
+        link: 'https://elpais.com/1.png',
+        image: 'https://image.elpais.com/1.png',
+      },
+      {
+        title: 'Elpais Title 2',
+        body: 'Elpais Body 2',
+        link: 'https://elpais.com/2.png',
+        image: 'https://image.elpais.com/1.png',
+      },
+    ];
+    const mockElmundoArticles: IArticle[] = [
+      {
+        title: 'Elmundo Title 1',
+        body: 'Elmundo Body 1',
+        link: 'https://elmundo.es/1.png',
+        image: 'https://image.elmundo.es/1.png',
+      },
+      {
+        title: 'Elmundo Title 2',
+        body: 'Elmundo Body 2',
+        link: 'https://elmundo.es/2.png',
+        image: 'https://image.elmundo.es/2.png',
+      },
+    ];
 
-        mockElpaisService.readFeeds.mockResolvedValue(mockElpaisArticles);
-        mockElmundoService.readFeeds.mockResolvedValue(mockElmundoArticles);
+    mockElpaisService.readFeeds.mockResolvedValue(mockElpaisArticles);
+    mockElmundoService.readFeeds.mockResolvedValue(mockElmundoArticles);
 
-        await scrapperService.getAndSaveAllFeeds();
+    await scrapperService.getAndSaveAllFeeds();
 
-        expect(mockElpaisService.readFeeds).toHaveBeenCalledTimes(2);
-    });
-
+    expect(mockElpaisService.readFeeds).toHaveBeenCalledTimes(2);
+  });
 });
