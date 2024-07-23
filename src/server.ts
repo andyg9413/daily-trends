b328883d77904b14f2ea1bf90abf07e40f9a4063
Andy <andy.gonzalez@opstalent.com>
2024-07-23 20:56:22 +0200

    feat: adding mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express, { Application } from 'express';
+import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Application = express();
 const PORT: string | number = process.env.PORT || 3000;
+const MONGO_DB_URI: string = process.env.MONGO_DB_URI || 'mongodb://localhost:27017/daily-trends';
+
+mongoose.connect(MONGO_DB_URI)
+  .then(() => console.log('DB is connected'))
+  .catch((err) => console.error(err));
 
 app.use(express.json());
 
